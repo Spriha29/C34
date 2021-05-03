3480bea606953b11480bc37deac10cd34e11b711
Spriha29 <76685656+Spriha29@users.noreply.github.com>
2021-05-03 15:49:12 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,32 @@
+class Box {
+	constructor(x,y,width,height)
+	{
+		var options={
+			'restitution':0.8,
+            'friction':1.0,
+            'density':0.04			
+			}
+
+		this.width=width;
+		this.height=height;
+		this.body=Bodies.rectangle(x, y, width, height, options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+            var angle = this.body.angle;
+			push()
+			translate(groundPos.x, groundPos.y);
+            rotate(angle);
+			rectMode(CENTER)
+			strokeWeight(4);
+            stroke("green");
+			fill(128,128,128);
+			rect(0,0,this.width, this.height);
+			pop();
+			
+	}
+}
